99c9637acc512ff6000d6697cd3aa735cd697432
kyokoshima <dev.kyoko@gmail.com>
2014-07-14 19:03:20 +0900

    split recipe

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures hbase'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w[java].each do |dep|
+%w[java ant].each do |dep|
 	depends dep
 end
